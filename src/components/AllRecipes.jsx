8b030d759a3a4c859bc3428603d8f5956abe7d71
Ashahab007 <ashahab007@gmail.com>
2025-08-31 01:32:02 +0600

    changes the layout

--- src/components/AllRecipes.jsx
+++ src/components/AllRecipes.jsx
@@ -1,18 +1,31 @@
-import React, { use, useState } from "react";
+import React, { useContext, useState } from "react";
 import RecipeCard from "./RecipeCard";
-import AuthContext from "./context/AuthContext";
 import { useLoaderData } from "react-router";
+import bgAllRecipeImg from "../assets/images/bg-all-recipe.jpg";
+import AuthContext from "./context/AuthContext";
 
 const AllRecipes = () => {
   const initialRecipes = useLoaderData();
   const [recipes, setRecipes] = useState(initialRecipes);
+  const { theme, setTheme } = useContext(AuthContext);
 
   // const { recipes, setRecipes } = use(AuthContext);
   console.log(recipes);
   return (
-    <div className="mt-20 mb-40">
-      <h1 className="text-center text-6xl font-bold">All Recipes</h1>
-      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
+    <div
+      className={`mt-4 mb-20 max-w-7xl mx-auto  ${
+        theme ? "dark" : ""
+      }  dark:bg-zinc-600`}
+    >
+      <div
+        style={{ backgroundImage: `url(${bgAllRecipeImg})` }}
+        className="relative w-full h-48 sm:h-64 md:h-80 bg-cover bg-no-repeat bg-center"
+      >
+        <h1 className="absolute top-[40%] pl-5 text-gray-300 w-full text-6xl font-bold">
+          All Recipes
+        </h1>
+      </div>
+      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4 mt-4">
         {recipes?.map((recipe) => (
           <RecipeCard recipe={recipe}></RecipeCard>
         ))}
